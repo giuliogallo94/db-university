d8d7c75514c2c0aa1397cef75ce8f759b540edfa
giuliogallo94 <giuliogallo1994@gmail.com>
2023-12-21 16:25:42 +0100

    add: query with GROUP BY

--- db_query.txt.sql
+++ db_query.txt.sql
@@ -117,3 +117,26 @@ WHERE `departments`.`name` = "Dipartimento di Matematica"
 
 -- Query with GROUP BY to execute
 
+-- 1. Contare quanti iscritti ci sono stati ogni anno
+SELECT YEAR(`enrolment_date`) as `year`, COUNT(*) as `total_enrolment`
+FROM `students`
+GROUP BY YEAR(`enrolment_date`)
+
+-- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+SELECT `office_address` as `address`, COUNT(*) as `number_of_teachers`
+FROM `teachers`
+GROUP BY `office_address`
+
+-- 3. Calcolare la media dei voti di ogni appello d'esame
+SELECT `exam_id` as `exam_session`, AVG(`vote`) as `avg_vote` 
+FROM `exam_student`
+GROUP BY `exam_id`  
+ORDER BY `exam_student`.`exam_id` ASC
+
+-- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+
+SELECT  `departments`.`name` as `department`, COUNT(*) as `number_of_degrees`
+FROM `degrees`
+JOIN `departments`
+ON `departments`.`id` = `degrees`.`department_id`
+GROUP BY `department_id`
